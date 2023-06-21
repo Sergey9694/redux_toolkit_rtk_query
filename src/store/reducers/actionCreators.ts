f04b73737351907ed6773303d1dc621c92fbe2ee
Sergey Drobilenko <drobilenko94@mail.ru>
2023-06-21 16:06:06 +0300

    функция для получения данных и обработки ошибки

---
+++ src/store/reducers/actionCreators.ts
@@ -0,0 +1,17 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { IUser } from "../../models/IUser";
+
+export const fetchUsers = createAsyncThunk(
+    "user/fetchAll",
+    async (_, thunkAPI) => {
+        try {
+            const response = await axios.get<IUser[]>(
+                "https://jsonplaceholder.typicode.com/uses"
+            );
+            return response.data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue("Не удалось загрузить данные");
+        }
+    }
+);
