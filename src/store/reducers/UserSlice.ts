34316b0cf676dcc7d4c08c2a246c68c5d5c5b33a
Sergey Drobilenko <drobilenko94@mail.ru>
2023-06-21 16:07:21 +0300

    логика по изменению данных

--- src/store/reducers/UserSlice.ts
+++ src/store/reducers/UserSlice.ts
@@ -1,5 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { IUser } from "../../models/IUser";
+import { fetchUsers } from "./actionCreators";
 
 interface UserState {
     users: IUser[];
@@ -18,7 +19,26 @@ const initialState: UserState = {
 export const userSlice = createSlice({
     name: "user", //уникальное название должно быть у каждого слайса
     initialState, //сюда - дефолтное значение состояния
+
+    //тут создаем функции внутри которых как-то изменяем состояние
     reducers: {},
+    extraReducers: {
+        [fetchUsers.pending.type]: (state) => {
+            state.isLoading = true;
+        },
+        [fetchUsers.fulfilled.type]: (
+            state,
+            action: PayloadAction<IUser[]>
+        ) => {
+            state.isLoading = false;
+            state.error = "";
+            state.users = action.payload;
+        },
+        [fetchUsers.rejected.type]: (state, action: PayloadAction<string>) => {
+            state.isLoading = false;
+            state.error = action.payload;
+        },
+    },
 });
 
 //Можно отдельно вытащить отдельно редюсер и отдельно экшн креэйторы
