65768a62d24f1ba6a4a12d159a2f4bda7774956c
Sergey Drobilenko <drobilenko94@mail.ru>
2023-06-21 16:07:45 +0300

    получение данных и отрисовка на странице

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "./hooks/redux";
+import { userSlice } from "./store/reducers/UserSlice";
+import { fetchUsers } from "./store/reducers/actionCreators";
 
 const App = () => {
-    return <div>Hello</div>;
+    const { users, isLoading, error } = useAppSelector(
+        (state) => state.userReducer
+    );
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(fetchUsers());
+    }, []);
+
+    return (
+        <div>
+            {isLoading && <h1>Идет загрузка данных</h1>}
+            {error && <h1>{error}</h1>}
+            {JSON.stringify(users)}
+        </div>
+    );
 };
 
 export default App;
